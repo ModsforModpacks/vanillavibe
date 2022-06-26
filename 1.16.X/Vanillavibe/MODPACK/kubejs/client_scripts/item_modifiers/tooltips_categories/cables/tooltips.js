140ff80206e49f3ed730b026c54700e12dabe2c5
szumaster <corrupted.souls99@gmail.com>
2022-06-27 01:37:20 +0200

    #11 [27/06/2022]
    japierdole

--- 1.16.X/Vanillavibe/MODPACK/kubejs/client_scripts/item_modifiers/tooltips_categories/cables/tooltips.js
+++ 1.16.X/Vanillavibe/MODPACK/kubejs/client_scripts/item_modifiers/tooltips_categories/cables/tooltips.js
@@ -9,52 +9,51 @@ onEvent('item.tooltip', tooltip => {
 //║              CABLE TIER                ║
 //╚════════════════════════════════════════╝
   tooltip.addAdvanced('techreborn:copper_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 128').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 128 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Medium').color('#FFAA00'))
     text.add(3, Text.of('Can kill').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:tin_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 32').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 32 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Low').color('#FFAA00'))
     text.add(3, Text.of('Can kill').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:gold_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 512').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 512 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: High').color('#FFAA00'))
     text.add(3, Text.of('Can kill').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:hv_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 2048').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 2048 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Extreme').color('#FFAA00'))
     text.add(3, Text.of('Can kill').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:glassfiber_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 8192').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 8192 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Insane').color('#FFAA00'))
-    text.add(3, Text.of('Can kill').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:insulated_copper_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 128').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 128 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: medium').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:insulated_gold_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 512').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 512 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: High').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:insulated_hv_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 2048').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 2048 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Extreme').color('#FFAA00'))
   });
 
   tooltip.addAdvanced('techreborn:superconductor_cable', (item, advanced, text) => {
-    text.add(1, Text.of('Transfer rate: 8192').color('#FFAA00'))
+    text.add(1, Text.of('Transfer rate: 8192 E/t').color('#FFAA00'))
     text.add(2, Text.of('Tier: Infinite').color('#FFAA00'))
   });
 
